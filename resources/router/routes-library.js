ab4b1d0f80053c269355a61c6b29ed27e3ce0ceb
Jinnyzxc <jinnielivonb@gmail.com>
2023-11-02 12:58:34 +0800

    add functionality of seller

--- resources/router/routes-library.js
+++ resources/router/routes-library.js
@@ -1,25 +1,23 @@
-import { createRouter, createWebHistory }  from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import LoginComponent from '../components/login.vue';
 import Home from '../components/home.vue';
 import NotFound from '../components/notFound.vue';
 import SignupComponent from '../components/signup.vue';
 import ForgotComponent from '../components/forgot.vue';
 import ResetComponent from '../components/reset.vue';
-import SellerPersonalInfo from './../components/seller/signup-seller.vue'
 import BuyerPersonalInfo from './../../resources/components/buyer/signup-buyer.vue'
-import DashboardSeller from './../components/seller/dashboardSeller.vue'
 import AdminDashboard from './../components/admin/dashboard.vue'
 import ShoppingList from './../components/buyer/shoppingList.vue'
 import ShoppingCart from './../components/buyer/shoppingCart.vue'
 import AdminVerifyBuyer from './../components/admin/verificationSeller.vue'
 import AdminVerifySeller from './../components/admin/verificationBuyer.vue'
-
-
+//seller components
+import SellerPersonalInfo from './../components/seller/signup-seller.vue'
+import DashboardSeller from './../components/seller/dashboardSeller.vue'
+import product from './../components/seller/products/viewProductList.vue'
+import shipping from './../components/seller/shipping/viewShipmentList.vue'
+import finance from './../components/seller/finance/viewFinance.vue'
 const routes = [
-    {
-        path: '/signup',
-        component: SignupComponent
-    },
     {
         path: '/admin',
         component: AdminDashboard
@@ -39,18 +37,34 @@ const routes = [
     {
         path: '/shopping-cart',
         component: ShoppingCart
-    },    
-    {
-        path: '/signup/seller',
-        component: SellerPersonalInfo
     },
     {
         path: '/seller/dashboard',
         component: DashboardSeller
     },
     {
+        path: '/seller/product',
+        component: product
+    },
+    {
+        path: '/seller/shipment',
+        component: shipping
+    },
+    {
+        path: '/seller/finance',
+        component: finance
+    },
+    {
+        path: '/signup/seller',
+        component: SellerPersonalInfo
+    },
+    {
         path: '/signup/buyer',
         component: BuyerPersonalInfo
+    },
+    {
+        path: '/signup',
+        component: SignupComponent
     },
     {
         path: '/login',
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,17 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>sangpum</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@100" rel="stylesheet" />
  
         @vite(['resources/css/app.css','resources/scss/app.scss', 'resources/js/app.js'])
     </head>
     <body>
-        <div id="app"></div>
+    <div id="app"></div>
     </body>
 </html>
